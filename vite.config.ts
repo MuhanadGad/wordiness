e2ba5f73e40e0ab5c633af9ca421eafad9b011ae
MuhanadGad <muhanad.gad@gmail.com>
2023-08-14 23:42:59 +0300

    s

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/wordiness',
+  base:'https://muhanadgad.github.io/wordiness',
   plugins: [react()],
 })
