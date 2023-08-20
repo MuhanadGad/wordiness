c8fe17e0aac8d4427b7a8c77eb10589e597a09b8
MuhanadGad <muhanad.gad@gmail.com>
2023-08-20 10:50:33 +0300

    fixed backspace

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -213,7 +213,7 @@ const Game = forwardRef((_props, ref) => {
 
   const keyboardLayout = {
     default: [
-      "q w e r t y u i o p {bksp}",
+      "q w e r t y u i o p {backspace}",
       "a s d f g h j k l {enter}",
       "z x c v b n m",
     ],
